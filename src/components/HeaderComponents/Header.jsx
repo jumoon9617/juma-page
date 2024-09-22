275afe4ca3a8e6cfbb721de72f0b9f4fffd92356
jumoon9617 <kazma_mole@icloud.com>
2024-09-22 20:45:11 +0900

    formatし忘れ

--- src/components/HeaderComponents/Header.jsx
+++ src/components/HeaderComponents/Header.jsx
@@ -6,9 +6,7 @@ function Header() {
       <nav className="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark py-3 px-5" data-bs-theme="dark">
         <div className="container-fluid pl-5">
           <a href="/" className="navbar-brand">
-            <div className={styles["nav-brand"]}>
-              JUMA
-            </div>
+            <div className={styles['nav-brand']}>JUMA</div>
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -16,22 +14,22 @@ function Header() {
           <div className="collapse navbar-collapse" id="navbarToggler">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a href="/about" className={`nav-link ${styles["nav-link"]}`}>
+                <a href="/about" className={`nav-link ${styles['nav-link']}`}>
                   About
                 </a>
               </li>
               <li className="nav-item">
-                <a href="/projects" className={`nav-link ${styles["nav-link"]}`}>
+                <a href="/projects" className={`nav-link ${styles['nav-link']}`}>
                   Projects
                 </a>
               </li>
               <li className="nav-item">
-                <a href="/blogs" className={`nav-link ${styles["nav-link"]}`}>
+                <a href="/blogs" className={`nav-link ${styles['nav-link']}`}>
                   Blogs
                 </a>
               </li>
               <li className="nav-item">
-                <a href="/contact" className={`nav-link ${styles["nav-link"]}`}>
+                <a href="/contact" className={`nav-link ${styles['nav-link']}`}>
                   Contact
                 </a>
               </li>
