cae889d0f5358e3b19e348ea26171532a60443c2
jumoon9617 <kazma_mole@icloud.com>
2024-09-24 00:04:18 +0900

    フッターの変更を一旦コミット

--- src/components/FooterComponents/Footer.jsx
+++ src/components/FooterComponents/Footer.jsx
@@ -3,7 +3,14 @@ import styles from './Footer.module.css'
 function Footer() {
   return (
     <div className={styles.root}>
-      <h1>Footer</h1>
+      <div className={styles.name}>KAZUMA HORIE</div>
+      <div className={styles.sns}>
+        SNS Links:
+        <div className={styles['sns-link']}>
+          <i className="bx bxl-twitter"></i>
+          <i className="bx bxl-github"></i>
+        </div>
+      </div>
     </div>
   )
 }
