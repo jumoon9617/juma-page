a76dc73eb6c45c6833505ee9faa6d33f62d01b1a
jumoon9617 <kazma_mole@icloud.com>
2024-09-24 00:37:05 +0900

    名前、アカウント、著作権情報を記載

--- src/components/FooterComponents/Footer.jsx
+++ src/components/FooterComponents/Footer.jsx
@@ -3,14 +3,15 @@ import styles from './Footer.module.css'
 function Footer() {
   return (
     <div className={styles.root}>
-      <div className={styles.name}>KAZUMA HORIE</div>
+      <div className={styles.name}>kaz</div>
       <div className={styles.sns}>
-        SNS Links:
+        <div>Acounts : </div>
         <div className={styles['sns-link']}>
           <i className="bx bxl-twitter"></i>
           <i className="bx bxl-github"></i>
         </div>
       </div>
+      <div className={styles.copyrights}>&copy; 2024 kaz. All rights reserved.</div>
     </div>
   )
 }
